8878643ba73aee9c39884a01150494f101c908a6
arang <friend9713@naver.com>
2023-01-16 19:40:04 +0900

    Mobago REST API server project created & settings
    settings :
     - jdbc
     - mysql driver
     - mybatis
     - lombok
     - spring web
     - spring boot dev tool

---
+++ src/main/java/com/arang/mobago/MobagoPjtApplication.java
@@ -0,0 +1,13 @@
+package com.arang.mobago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobagoPjtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobagoPjtApplication.class, args);
+    }
+
+}
